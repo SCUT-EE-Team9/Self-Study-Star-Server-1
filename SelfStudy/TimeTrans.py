3399bac330d100ac01d66fcc5c56916d4ecfe450
Diaplo <70957228+Diaplo@users.noreply.github.com>
2020-12-12 01:20:54 +0800

    时间转换改动+查找
    修改时间转换函数
    添加查找函数

--- SelfStudy/TimeTrans.py
+++ SelfStudy/TimeTrans.py
@@ -1,25 +1,22 @@
 import time
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from config import *
+import pymysql
 
 
 def unix_time(dt):
+    if(dt=="") :return -1
     # 转换成时间数组
     timeArray = time.strptime(dt, "%Y-%m-%dT%H:%M")
     # 转换成时间戳
     unixtime = time.mktime(timeArray)
-    return unixtime
+    return int(unixtime)
 
 
-def normol_time(unixtime):
-    # 转换成normoltime
+def normal_time(unixtime):
+    # 转换成normaltime
     time_local = time.localtime(unixtime)
-    # 转换成新的时间格式(2016-05-05T20:28)
-    dt = time.strftime("%Y-%m-%dT%H:%M", time_local)
+    # 转换成新的时间格式(2016年05月05日20:28)
+    dt = time.strftime("%Y年%m月%d日%H:%M", time_local)
     return dt
-
-
-if __name__ == '__main__':
-    str = input("enter a time\n")
-    unix_t = unix_time(str)
-    normol_t = normol_time(unix_t)
-    print(unix_t)
-    print(normol_t)
--- SelfStudy/app.py
+++ SelfStudy/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from users import *
+from TimeTrans import *
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,47 +11,102 @@ class IndexView(Resource):
     def get(self):
         return {"hello": "world123"}
 
+    #
+    """
 
-'''class UserAPI(Resource):
-    def get(self, uid):
-        return {"user": uid}
-
-    def put(self, uid):
-        pass
-
-    def delete(self, uid):
-        pass
+    """
 
 
-class Publish(Resource):
+class TestSearch(Resource):
     def get(self):
-        return {"error": "method not allowed"}
+        return {"hello": "world123"}
 
     def post(self):
-        parser = reqparse.RequestParser()  # 新建parser实例
-        # 向parser实例中添加rate参数，并加以配置
-        # parser.add_argument('rate', type=float, help='Rate cannot be converted')
-        parser.add_argument('username')
-        parser.add_argument('title')
-        parser.add_argument('content')
+        return {"1": {"tid": "19", "username": "zhangsan", "stime": "2020111111:11", "etime": "2020111112:11",
+                      "location": "210", "remarks": "hswt", "joined": "0", "finished": "1"},
+                "2": {"tid": "20", "username": "zhangsan", "stime": "2020111111:11", "etime": "2020111112:11",
+                      "location": "210", "remarks": "hswt", "joined": "0", "finished": "0"},
+                "3": {"tid": "21", "username": "zhangsan", "stime": "2020111111:11", "etime": "2020111112:11",
+                      "location": "210", "remarks": "hswt", "joined": "0", "finished": "0"},
+                "4": {"tid": "22", "username": "zhangsan", "stime": "2020111111:11", "etime": "2020111112:11",
+                      "location": "210", "remarks": "hswt", "joined": "0", "finished": "0"}
+                }
 
-        # 将请求中传过来的参数存到args中
-        args = parser.parse_args()
-        # 将请求参数中的rate的值加以计算，并返回
-        username= args["username"]
-        title= args["title"]
-        content= args["content"]
-        return {
-            "username": username,
-            "title": title,
-            "content": content
-            # "result": float(args["rate"]*100)
-        }
-'''
+
+''' return {
+	"result": "ok",
+	"data": {
+		"0": {
+			"tid": 19,
+			"username": "zhangsan",
+			"stime": "2020111111:11",
+			"etime": "2020111112:11",
+			"location": "210",
+			"remarks": "hswt",
+			"joined": 0,
+			"finished": "已完成"
+		},
+		"1": {
+			"tid": 20,
+			"username": "zhangsan",
+			"stime": "2020年11月11日11:11",
+			"etime": "2020年11月11日12:11",
+			"location": "博学楼210",
+			"remarks": "黑色外套",
+			"joined": 0,
+			"finished": "已完成"
+		},
+		"2": {
+			"tid": 21,
+			"username": "zhangsan",
+			"stime": "2020年11月11日11:11",
+			"etime": "2020年11月11日12:11",
+			"location": "博学楼210",
+			"remarks": "黑色外套\"",
+			"joined": 0,
+			"finished": "已完成"
+		},
+		"3": {
+			"tid": 22,
+			"username": "zhangsan",
+			"stime": "2020'年11月11日11:11",
+			"etime": "2020年11月11日12:11'",
+			"location": "博学楼210'",
+			"remarks": "黑色外套\"'",
+			"joined": 0,
+			"finished": "已完成"
+		},
+		"4": {
+			"tid": 23,
+			"username": "zhangsan",
+			"stime": "2020'年11月11日11:11",
+			"etime": "2020年11月11日12:11'",
+			"location": "博学楼210'",
+			"remarks": "黑色外套\"'",
+			"joined": 0,
+			"finished": "已完成"
+		},
+		"5": {
+			"tid": 24,
+			"username": "zhangsan",
+			"stime": "2020'年11月11日11:11",
+			"etime": "2020年11月11日12:11'",
+			"location": "博学楼210'",
+			"remarks": "黑色外套\"'",
+			"joined": 0,
+			"finished": "已完成"
+		}
+	}
+}'''
 
 api.add_resource(UserAPI, '/users/<int:uid>', endpoint='user')
 api.add_resource(IndexView, '/', endpoint='index')
 api.add_resource(Publish, '/publish', endpoint='Publish')
+api.add_resource(Search, '/search', endpoint='Search')
+api.add_resource(Join, '/join', endpoint='Join')
+api.add_resource(MyPublished, '/my/published', endpoint='MyPublished')
+api.add_resource(MyJoined, '/my/joined', endpoint='MyJoined')
+api.add_resource(TestSearch, '/test/search', endpoint='testsearch')
 
 
 def after_request(resp):
@@ -61,6 +117,5 @@ def after_request(resp):
 
 app.after_request(after_request)
 
-
-#if __name__ == '__main__':
-#    app.run()
+if __name__ == '__main__':
+   app.run()
